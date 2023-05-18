9b43a41ade53454907380bf45a3e727e2d926541
Akshay kumar <akshay.p.kumar@happiestminds.com>
2023-05-18 16:14:30 +0530

    topbaradded

--- app/src/main/java/com/example/articalsdemo/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/articalsdemo/ui/activity/MainActivity.kt
@@ -3,8 +3,23 @@ package com.example.articalsdemo.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.articalsdemo.model.Artical
 import com.example.articalsdemo.repo.ArticalRepository
 import com.example.articalsdemo.ui.adapter.CustomArtical
@@ -15,11 +30,30 @@ class MainActivity :ComponentActivity() {
         setContent {
             val articalRepository = ArticalRepository()
             val getAllData = articalRepository.getAllData()
+
             LazyColumn(){
                 items(items =getAllData){
                         Artical ->  CustomArtical(artical = Artical)
                 }
             }
+
+
+            TopAppBar(
+                title = {
+                    Text( modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start,text = "Articles")
+
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.KeyboardArrowLeft, "backIcon")
+                    }
+                },
+                backgroundColor = Color.White,
+                contentColor = Color.Black
+            )
+
+
         }
     }
 }
--- app/src/main/java/com/example/articalsdemo/ui/adapter/CustomArtical.kt
+++ app/src/main/java/com/example/articalsdemo/ui/adapter/CustomArtical.kt
@@ -1,17 +1,26 @@
 package com.example.articalsdemo.ui.adapter
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,9 +41,9 @@ import androidx.compose.ui.unit.sp
 import com.example.articalsdemo.R
 import com.example.articalsdemo.model.Artical
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun CustomArtical(artical: Artical){
-
     Column() {
         Image  (
             painter = painterResource(artical.ImageId),
@@ -43,21 +52,16 @@ fun CustomArtical(artical: Artical){
                 .height(267.dp),
             contentScale = ContentScale.FillBounds,
             contentDescription = ""
-
-
         )
-
         Text(text = artical.title,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .background(color = Color.White)
                 .padding(20.dp)
                 .wrapContentHeight(align = Alignment.CenterVertically),
                 color = Color.Black,fontSize = 20.sp, fontStyle = FontStyle.Normal,textAlign = TextAlign.Start)
-
     }
-
 }
-
 @Composable
 @Preview
 fun CustomItemPreview() {
